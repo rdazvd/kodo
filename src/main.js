a0fc3b2b4d505c9d5d5c55379def07be348a01c6
rdazvd <rdazvd@users.noreply.github.com>
2019-10-13 16:59:18 -0300

    parseFont function created in main.js

--- src/main.js
+++ src/main.js
@@ -1,28 +1,43 @@
-import { font } from './font/fontDescription';
+import { font } from "./font/fontDescription";
 
-const buffer = font.toArrayBuffer();
-const parsedFont = opentype.parse(buffer);
+const parseFont = font => {
+  const buffer = font.toArrayBuffer();
+  return opentype.parse(buffer);
+};
+
+const parsedFont = parseFont(font);
 
 function createGlyphCanvas(glyph, size) {
   let canvasId, html, glyphsDiv, wrap, canvas, ctx;
-  canvasId = 'c' + glyph.index;
-  html = '<div class="wrapper" style="width:' + size + 'px"><canvas id="' + canvasId + '" width="' + size + '" height="' + size + '"></canvas><span>' + glyph.index + '</span></div>';
-  glyphsDiv = document.getElementById('glyphs');
-  wrap = document.createElement('div');
+  canvasId = "c" + glyph.index;
+  html =
+    '<div class="wrapper" style="width:' +
+    size +
+    'px"><canvas id="' +
+    canvasId +
+    '" width="' +
+    size +
+    '" height="' +
+    size +
+    '"></canvas><span>' +
+    glyph.index +
+    "</span></div>";
+  glyphsDiv = document.getElementById("glyphs");
+  wrap = document.createElement("div");
   wrap.innerHTML = html;
   glyphsDiv.appendChild(wrap);
   canvas = document.getElementById(canvasId);
-  ctx = canvas.getContext('2d');
+  ctx = canvas.getContext("2d");
   return ctx;
 }
 
 for (let i = 1; i < parsedFont.glyphs.length; i++) {
-    let glyph = parsedFont.glyphs.get(i);
-    let ctx = createGlyphCanvas(glyph, 500);
-    let x = 50;
-    let y = 400;
-    let fontSize = 500;
-    glyph.draw(ctx, x, y, fontSize);
-    glyph.drawPoints(ctx, x, y, fontSize);
-    glyph.drawMetrics(ctx, x, y, fontSize);
-};
+  let glyph = parsedFont.glyphs.get(i);
+  let ctx = createGlyphCanvas(glyph, 500);
+  let x = 50;
+  let y = 400;
+  let fontSize = 500;
+  glyph.draw(ctx, x, y, fontSize);
+  glyph.drawPoints(ctx, x, y, fontSize);
+  glyph.drawMetrics(ctx, x, y, fontSize);
+}
